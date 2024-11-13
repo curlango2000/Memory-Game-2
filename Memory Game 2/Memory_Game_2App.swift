4dd88955e8b4a14d1428c0fa19a98e36eaba8da5
curlango <curlango2000@gmail.com>
2024-11-13 10:56:04 -0800

    Initial Commit

---
+++ Memory Game 2/Memory_Game_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_Game_2App.swift
+//  Memory Game 2
+//
+//  Created by Cecilia Curlango on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_Game_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
